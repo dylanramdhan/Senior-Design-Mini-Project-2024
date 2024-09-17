bcf833d8c9ff5fa818d11e602a3b097fe1b86b71
Beren Yagmur Donmez <bydonmez@bu.edu>
2024-09-17 19:16:59 -0400

    syntax and fixes pushing theme song

--- EXERCISES/harry_potter.py
+++ EXERCISES/harry_potter.py
@@ -1,9 +1,7 @@
 import machine
 import utime
 
-
 SPEAKER_PIN = 16
-
 
 speaker = machine.PWM(machine.Pin(SPEAKER_PIN))
 
@@ -18,14 +16,12 @@ NOTE_FREQUENCIES = {
     'F#6': 1480,
 }
 
-
 DylanAndBerenTheme = [
     ('E5', 0.5), ('G5', 0.5), ('F#5', 0.5), ('E5', 0.5),
     ('B4', 0.5), ('E5', 0.5), ('G5', 0.5), ('A5', 0.5),
     ('F#5', 0.5), ('E5', 0.5), ('B4', 0.5), ('E6', 0.5),
     ('D#6', 0.5), ('B4', 0.5), ('F#6', 0.5)
 ]
-
 
 def playtone(frequency: float, duration: float) -> None:
     """Plays a tone at the given frequency for the given duration."""
@@ -34,11 +30,9 @@ def playtone(frequency: float, duration: float) -> None:
     utime.sleep(duration)
     quiet()
 
-
 def quiet():
     """Stops the sound (duty cycle set to 0)."""
     speaker.duty_u16(0)
-
 
 print("Playing Harry Potter Hedwig's theme:")
 
@@ -47,3 +41,4 @@ for note, duration in DylanAndBerenTheme:
     playtone(NOTE_FREQUENCIES[note], duration)
 
 quiet()
+
